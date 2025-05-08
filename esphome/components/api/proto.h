8e5373c0a18a92cdfc5177b91b8c22310a967682
J. Nick Koston <nick@koston.org>
2025-05-07 19:37:04 -0500

    fixes

--- esphome/components/api/proto.h
+++ esphome/components/api/proto.h
@@ -13,9 +13,6 @@
 namespace esphome {
 namespace api {
 
-// Function to benchmark and log protobuf encoding times
-void log_protobuf_encode_timing(const ProtoMessage &msg);
-
 /// Representation of a VarInt - in ProtoBuf should be 64bit but we only use 32bit
 class ProtoVarInt {
  public:
@@ -305,6 +302,8 @@ class ProtoService {
   virtual ProtoWriteBuffer create_buffer() = 0;
   virtual bool send_buffer(ProtoWriteBuffer buffer, uint32_t message_type) = 0;
   virtual bool read_message(uint32_t msg_size, uint32_t msg_type, uint8_t *msg_data) = 0;
+  // Function to benchmark and log protobuf encoding times
+  void log_protobuf_encode_timing(const ProtoMessage &msg);
 
   // Optimized method that pre-allocates buffer based on message size
   template<class C> bool send_message_(const C &msg, uint32_t message_type) {
